87a46e4b118cb9da2862146ad10ed5b4d2622dac
juliobarbagallo <40072754+juliobarbagallo@users.noreply.github.com>
2019-07-31 09:53:23 -0300

    charming changes
    Added func docstring, also played with pylint

--- tateti.py
+++ tateti.py
@@ -1,16 +1,21 @@
+import random
+
 # We start creating the board, it will be a list of 9 positions. It starts with all blanks ' '.
 board = [' ' for x in range(10)]
 
 
-def insert_letter(letter, pos):  # Insert etter (X or O) in given position.
+def insert_letter(letter, pos):
+    """Insert letter (X or O) in given position."""
     board[pos] = letter
 
 
-def pos_is_free(pos):  # Boolean function, return True if the position on the board is available.
+def pos_is_free(pos):
+    """Boolean function, return True if the position on the board is available."""
     return board[pos] == ' '
 
 
-def print_board(board):  # It just make the prints to reflect a ta-te-ti board.
+def print_board(board):
+    """It just make the prints to reflect a ta-te-ti board."""
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
@@ -23,12 +28,11 @@ def print_board(board):  # It just make the prints to reflect a ta-te-ti board.
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
-#  s_winner funct get a board and a letter and checks if horizontal lines (7,8 and 9) or
-#  (4,5 and 6) or ( 1,2 and 3) has the same letter (X or O) or if verticals lines (1,4 and 7) or
-#  (2, 5 and 8) or (3, 6 and 9) has the same letter. And, also for diagonals (1, 5 and 9) or (2, 5 and 7)
-
 
 def is_winner(bo, le):
+    """It gets a board and a letter and checks if horizontal lines (7,8 and 9) or
+    (4,5 and 6) or ( 1,2 and 3) has the same letter (X or O) or if verticals lines (1,4 and 7) or
+    (2, 5 and 8) or (3, 6 and 9) has the same letter. And, also for diagonals (1, 5 and 9) or (2, 5 and 7)"""
     return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or (
                 bo[1] == le and bo[2] == le and bo[3] == le) or (bo[1] == le and bo[4] == le and bo[7] == le) or (
                        bo[2] == le and bo[5] == le and bo[8] == le) or (
@@ -36,7 +40,8 @@ def is_winner(bo, le):
                        bo[1] == le and bo[5] == le and bo[9] == le) or (bo[3] == le and bo[5] == le and bo[7] == le)
 
 
-def player_move():  # It tries to set the letter X (player) to desired board position.
+def player_move():
+    """It tries to set the letter X (player) to desired board position."""
     run = True
     while run:  # Until run equals to True, ask the user for the position.
         move = input('Please select a position to place an \'X\' (1-9): ')
@@ -54,7 +59,8 @@ def player_move():  # It tries to set the letter X (player) to desired board pos
             print('Please type a number!')
 
 
-def machine_move():  # It tries the machine move. As its request, this point is deeper explained at the README file.
+def machine_move():
+    """It tries the machine move. As its request, this point is deeper explained at the README file."""
     possible_moves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
 
@@ -91,38 +97,45 @@ def machine_move():  # It tries the machine move. As its request, this point is 
 
 
 def pick_random(li):
-    import random
+    """From a list of available positions, it randomly pick
+    one of them to make the machine move."""
+    #import random
     ln = len(li)
     r = random.randrange(0, ln)
     return li[r]
 
 
 def is_board_full(board):
+    """It use count function in order to count how many blanks (' ')
+    are in the board and return True if there aren't blank spaces
+    or False if there is any."""
     if board.count(' ') > 1:
         return False
-    else:
-        return True
+    return True
 
 
 def main():
+    """This is the main function, which will call each functions
+    in order to play the game. It also got the logic for the
+    random init between user and machine"""
     from random import randrange
     print('Ta-Te-Ti')
     print_board(board)
 
     player_turn = randrange(2)
 
-    while not (is_board_full(board)):
+    while not is_board_full(board):
         if player_turn == 0:
-            if not (is_winner(board, 'O')):
+            if not is_winner(board, 'O'):
                 player_move()
                 print_board(board)
             else:
-                print('Sorry, O\'s won this time!')
+                print('Sorry, computer won this time!')
                 break
 
         player_turn = 0
 
-        if not (is_winner(board, 'X')):
+        if not is_winner(board, 'X'):
             move = machine_move()
             if not move == 0:
             #     print('Tie Game!')
@@ -131,15 +144,16 @@ def main():
                 print('Computer placed an \'O\' in position', move, ':')
                 print_board(board)
         else:
-            if not (is_winner(board, 'X')) and not (is_board_full(board)):
+            if not is_winner(board, 'X') and not is_board_full(board):
                 print('Tie Game!')
                 break
             else:
-                print('X\'s won this time! Good Job!')
+                print('You won this time! Good Job!')
                 break
 
     # if is_board_full(board):
     #     print('Tie Game!')
+
 
 if __name__ == '__main__':
     main()
